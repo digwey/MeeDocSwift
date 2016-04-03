15a4fbc9f1fed794f918f0d8c456ccfa623b62ae
Mohamed Digwey <mohamed.eldigwey@vodafone.com>
2016-04-03 08:56:25 +0200

    This is for initialize the project

---
+++ MeeDocClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeeDocClient
+//
+//  Created by Mohamed Digwey on 4/3/16.
+//  Copyright © 2016 Mohamed Digwey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
